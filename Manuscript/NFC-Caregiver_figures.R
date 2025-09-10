fdcaaa998ce27ab9c2cecdb0724c7b1b2714566c
Alex Strobel <87649655+alex-strobel@users.noreply.github.com>
2025-09-10 10:55:44 +0200

    Update Ms.

--- Manuscript/NFC-Caregiver_figures.R
+++ Manuscript/NFC-Caregiver_figures.R
@@ -1,58 +1,270 @@
-par(mar=c(0,0,0,0))
-plot(c(0,20), c(0,12), type="n", axes=T, xlab="", ylab="")
-rect(0,5.5,4,6.5) 
-text(2,6, "Need for\nCognition", cex=.8)
-rect(8,5.5,12,6.5)
-text(10,6, "Suppression", cex=.8)
-rect(8,7.5,12,8.5)
-text(10,8, "Reappraisal", cex=.8)
-rect(8,9.5,12,10.5)
-text(10,10, "Self-Control", cex=.8)
-rect(8,3.5,12,4.5)
-text(10,4, "Adaptive\nCoping", cex=.8)
-rect(8,1.5,12,2.5)
-text(10,2, "Maladaptive\nCoping", cex=.8)
-rect(16,5.5,20,6.5)
-text(18,6, "Reduced\nPersonal Efficiency", cex=.8)
-Arrows(4,6,8,6, arr.type = "triangle", arr.adj = 1, arr.length = .2, arr.width = .2)
-lines(rep(5,2), c(2,10))
-Arrows(5,2,8,2, arr.type = "triangle", arr.adj = 1, arr.length = .2, arr.width = .2)
-Arrows(5,4,8,4, arr.type = "triangle", arr.adj = 1, arr.length = .2, arr.width = .2)
-Arrows(5,8,8,8, arr.type = "triangle", arr.adj = 1, arr.length = .2, arr.width = .2)
-Arrows(5,10,8,10, arr.type = "triangle", arr.adj = 1, arr.length = .2, arr.width = .2)
-Arrows(12,6,16,6, arr.type = "triangle", arr.adj = 1, arr.length = .2, arr.width = .2)
-lines(rep(15,2), c(2,10))
-lines(c(12,15), rep(2,2))
-lines(c(12,15), rep(4,2))
-lines(c(12,15), rep(8,2))
-lines(c(12,15), rep(10,2))
-rect(6,1.5,7,10.5, border = NA, col = 0)
-rect(13,1.5,14,10.5, border = NA, col = 0)
-# paths a
-text(6.5,10,printnum(pe_rpe$std.all[which(pe_rpe$label=="a1")], gt1=F), cex=.8)
-text(6.5,8,printnum(pe_rpe$std.all[which(pe_rpe$label=="a2")], gt1=F), cex=.8)
-text(6.5,6,printnum(pe_rpe$std.all[which(pe_rpe$label=="a3")], gt1=F), cex=.8)
-text(6.5,4,printnum(pe_rpe$std.all[which(pe_rpe$label=="a4")], gt1=F), cex=.8)
-text(6.5,2,printnum(pe_rpe$std.all[which(pe_rpe$label=="a5")], gt1=F), cex=.8)
+library(shape)   # for Arrows()
 
-# paths b
-text(13.5,10,printnum(pe_rpe$std.all[which(pe_rpe$label=="b1")], gt1=F), cex=.8)
-text(13.5,8,printnum(pe_rpe$std.all[which(pe_rpe$label=="b2")], gt1=F), cex=.8)
-text(13.5,6,printnum(pe_rpe$std.all[which(pe_rpe$label=="b3")], gt1=F), cex=.8)
-text(13.5,4,printnum(pe_rpe$std.all[which(pe_rpe$label=="b4")], gt1=F), cex=.8)
-text(13.5,2,printnum(pe_rpe$std.all[which(pe_rpe$label=="b5")], gt1=F), cex=.8)
+# load parameter estimates for rpe, ee, and de
 
-# paths ab
-text(10,10.7,printnum(pe_rpe$std.all[which(pe_rpe$label=="ind1")], gt1=F), cex=.8)
-text(10,8.7,printnum(pe_rpe$std.all[which(pe_rpe$label=="ind2")], gt1=F), cex=.8)
-text(10,6.7,printnum(pe_rpe$std.all[which(pe_rpe$label=="ind3")], gt1=F), cex=.8)
-text(10,4.7,printnum(pe_rpe$std.all[which(pe_rpe$label=="ind4")], gt1=F), cex=.8)
-text(10,2.7,printnum(pe_rpe$std.all[which(pe_rpe$label=="ind5")], gt1=F), cex=.8)
+# FIGURE 1 --------------------------------------------------------------------
 
-# path c/c'
-lines(rep(2,2),c(5.5, 0))
-lines(c(2,18),c(0, 0))
-Arrows(18,0,18,5.5, arr.type = "triangle", arr.adj = 1, arr.length = .2, arr.width = .2)
-text(10,0.2, printnum(pe_rpe$std.all[which(pe_rpe$label=="c")], gt1=F), cex=.8)
-#text(10,-0.2, paste0("(",printnum(pe_rpe$std.all[which(pe_rpe$label=="total")], gt1=F), ")"), cex=.8)
+# --- helper functions ---
+get_val <- function(lbl) {
+  printnum(pe_rpe$std.all[pe_rpe$label == lbl], gt1 = FALSE)
+}
+get_sig <- function(lbl) {
+  (pe_rpe$pvalue[pe_rpe$label == lbl] < .05) + 1
+}
+
+# --- layout parameters ---
+x_left   <- 0
+x_med    <- 8
+x_right  <- 16
+box_w    <- 4
+box_h    <- 1
+ind_offset <- .8
+
+# y-coordinates of mediator boxes
+y_mediators <- c(9.5, 7.75, 6, 4.25, 2.5)
+
+mediator_labels <- c(
+  "Self-Control",
+  "Reappraisal",
+  "Suppression",
+  "Adaptive\nCoping",
+  "Maladaptive\nCoping"
+)
+
+a_labels <- paste0("a", 1:5)
+b_labels <- paste0("b", 1:5)
+ind_labels <- paste0("ind", 1:5)
+
+# --- prepare plot ---
+par(mar = c(0,0,0,0))
+plot(c(0,20), c(0,10.75), type="n", axes=FALSE, xlab="", ylab="")
+
+# --- boxes ---
+# predictor
+rect(x_left, 5.5, x_left+box_w, 6.5)
+text(x_left+box_w/2, 6, "Need for\nCognition", cex=.8)
+
+# mediators
+for (i in seq_along(y_mediators)) {
+  rect(x_med, y_mediators[i]-box_h/2, x_med+box_w, y_mediators[i]+box_h/2)
+  text(x_med+box_w/2, y_mediators[i], mediator_labels[i], cex=.8)
+}
+
+# outcome
+rect(x_right, 5.5, x_right+box_w, 6.5)
+text(x_right+box_w/2, 6, "Reduced\nPersonal Efficiency", cex=.8)
+
+# --- arrows from predictor to mediators (a paths) ---
+lines(rep(5,2), range(y_mediators))
+lines(c(4,5), rep(6,2))
+for (i in seq_along(y_mediators)) {
+  lines(c(5,6), rep(y_mediators[i],2))
+  Arrows(7, y_mediators[i], x_med, y_mediators[i], arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+  text(6.5, y_mediators[i], get_val(a_labels[i]), font=get_sig(a_labels[i]), cex=.8)
+}
+
+# --- arrows from mediators to outcome (n paths) ---
+lines(rep(x_right-1,2), range(y_mediators))
+Arrows(x_right-1, 6, x_right, 6, arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+
+for (i in seq_along(y_mediators)) {
+  lines(c(x_med+4,x_med+5), rep(y_mediators[i], 2))
+  text(13.5, y_mediators[i], get_val(b_labels[i]), font=get_sig(b_labels[i]), cex=.8)
+  lines(c(x_med+6,x_med+7), rep(y_mediators[i], 2))
+}
+
+# --- indirect effects (a*b) ---
+for (i in seq_along(y_mediators)) {
+  text(x_med+box_w/2, y_mediators[i]+ind_offset, get_val(ind_labels[i]), 
+       font=get_sig(ind_labels[i]), cex=.8)
+}
+
+# --- direct effect (c/c') ---
+lines(rep(x_left+2,2),c(5.5, y_mediators[5]-1.75))
+lines(c(x_left+2,x_right+2), rep(y_mediators[5]-1.75, 2))
+Arrows(x_right+2, y_mediators[5]-1.75, x_right+2, y_mediators[3]-.5, arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+text(x_med+2,y_mediators[5]-1.75+.3, get_val("c"), font=get_sig("c"), cex=.8)
+text(x_med+2,y_mediators[5]-1.75-.3, paste0("[", get_val("total"), "]"), font=get_sig("total"), cex=.8)
+
+# reset plot margins
 par(mar=c(5,4,4,2))
+
+# now save figure as *.eps via Export > Save as Image ...
+
+# FIGURE 2 --------------------------------------------------------------------
+
+# --- helper functions ---
+get_val <- function(lbl) {
+  printnum(pe_ee$std.all[pe_ee$label == lbl], gt1 = FALSE)
+}
+get_sig <- function(lbl) {
+  (pe_ee$pvalue[pe_ee$label == lbl] < .05) + 1
+}
+
+# --- layout parameters ---
+x_left   <- 0
+x_med    <- 8
+x_right  <- 16
+box_w    <- 4
+box_h    <- 1
+ind_offset <- .8
+
+# y-coordinates of mediator boxes
+y_mediators <- c(9.5, 7.75, 6, 4.25, 2.5)
+
+mediator_labels <- c(
+  "Self-Control",
+  "Reappraisal",
+  "Suppression",
+  "Adaptive\nCoping",
+  "Maladaptive\nCoping"
+)
+
+a_labels <- paste0("a", 1:5)
+b_labels <- paste0("b", 1:5)
+ind_labels <- paste0("ind", 1:5)
+
+# --- prepare plot ---
+par(mar = c(0,0,0,0))
+plot(c(0,20), c(0,10.75), type="n", axes=FALSE, xlab="", ylab="")
+
+# --- boxes ---
+# predictor
+rect(x_left, 5.5, x_left+box_w, 6.5)
+text(x_left+box_w/2, 6, "Need for\nCognition", cex=.8)
+
+# mediators
+for (i in seq_along(y_mediators)) {
+  rect(x_med, y_mediators[i]-box_h/2, x_med+box_w, y_mediators[i]+box_h/2)
+  text(x_med+box_w/2, y_mediators[i], mediator_labels[i], cex=.8)
+}
+
+# outcome
+rect(x_right, 5.5, x_right+box_w, 6.5)
+text(x_right+box_w/2, 6, "Emotional\nExhaustion", cex=.8)
+
+# --- arrows from predictor to mediators (a paths) ---
+lines(rep(5,2), range(y_mediators))
+lines(c(4,5), rep(6,2))
+for (i in seq_along(y_mediators)) {
+  lines(c(5,6), rep(y_mediators[i],2))
+  Arrows(7, y_mediators[i], x_med, y_mediators[i], arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+  text(6.5, y_mediators[i], get_val(a_labels[i]), font=get_sig(a_labels[i]), cex=.8)
+}
+
+# --- arrows from mediators to outcome (n paths) ---
+lines(rep(x_right-1,2), range(y_mediators))
+Arrows(x_right-1, 6, x_right, 6, arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+
+for (i in seq_along(y_mediators)) {
+  lines(c(x_med+4,x_med+5), rep(y_mediators[i], 2))
+  text(13.5, y_mediators[i], get_val(b_labels[i]), font=get_sig(b_labels[i]), cex=.8)
+  lines(c(x_med+6,x_med+7), rep(y_mediators[i], 2))
+}
+
+# --- indirect effects (a*b) ---
+for (i in seq_along(y_mediators)) {
+  text(x_med+box_w/2, y_mediators[i]+ind_offset, get_val(ind_labels[i]), 
+       font=get_sig(ind_labels[i]), cex=.8)
+}
+
+# --- direct effect (c/c') ---
+lines(rep(x_left+2,2),c(5.5, y_mediators[5]-1.75))
+lines(c(x_left+2,x_right+2), rep(y_mediators[5]-1.75, 2))
+Arrows(x_right+2, y_mediators[5]-1.75, x_right+2, y_mediators[3]-.5, arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+text(x_med+2,y_mediators[5]-1.75+.3, get_val("c"), font=get_sig("c"), cex=.8)
+text(x_med+2,y_mediators[5]-1.75-.3, paste0("[", get_val("total"), "]"), font=get_sig("total"), cex=.8)
+
+# reset plot margins
+par(mar=c(5,4,4,2))
+
+# now save figure as *.eps via Export > Save as Image ...
+
+# FIGURE 3 --------------------------------------------------------------------
+
+# --- helper functions ---
+get_val <- function(lbl) {
+  printnum(pe_de$std.all[pe_de$label == lbl], gt1 = FALSE)
+}
+get_sig <- function(lbl) {
+  (pe_de$pvalue[pe_de$label == lbl] < .05) + 1
+}
+
+# --- layout parameters ---
+x_left   <- 0
+x_med    <- 8
+x_right  <- 16
+box_w    <- 4
+box_h    <- 1
+ind_offset <- .8
+
+# y-coordinates of mediator boxes
+y_mediators <- c(9.5, 7.75, 6, 4.25, 2.5)
+
+mediator_labels <- c(
+  "Self-Control",
+  "Reappraisal",
+  "Suppression",
+  "Adaptive\nCoping",
+  "Maladaptive\nCoping"
+)
+
+a_labels <- paste0("a", 1:5)
+b_labels <- paste0("b", 1:5)
+ind_labels <- paste0("ind", 1:5)
+
+# --- prepare plot ---
+par(mar = c(0,0,0,0))
+plot(c(0,20), c(0,10.75), type="n", axes=FALSE, xlab="", ylab="")
+
+# --- boxes ---
+# predictor
+rect(x_left, 5.5, x_left+box_w, 6.5)
+text(x_left+box_w/2, 6, "Need for\nCognition", cex=.8)
+
+# mediators
+for (i in seq_along(y_mediators)) {
+  rect(x_med, y_mediators[i]-box_h/2, x_med+box_w, y_mediators[i]+box_h/2)
+  text(x_med+box_w/2, y_mediators[i], mediator_labels[i], cex=.8)
+}
+
+# outcome
+rect(x_right, 5.5, x_right+box_w, 6.5)
+text(x_right+box_w/2, 6, "Depersonalization", cex=.8)
+
+# --- arrows from predictor to mediators (a paths) ---
+lines(rep(5,2), range(y_mediators))
+lines(c(4,5), rep(6,2))
+for (i in seq_along(y_mediators)) {
+  lines(c(5,6), rep(y_mediators[i],2))
+  Arrows(7, y_mediators[i], x_med, y_mediators[i], arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+  text(6.5, y_mediators[i], get_val(a_labels[i]), font=get_sig(a_labels[i]), cex=.8)
+}
+
+# --- arrows from mediators to outcome (n paths) ---
+lines(rep(x_right-1,2), range(y_mediators))
+Arrows(x_right-1, 6, x_right, 6, arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+
+for (i in seq_along(y_mediators)) {
+  lines(c(x_med+4,x_med+5), rep(y_mediators[i], 2))
+  text(13.5, y_mediators[i], get_val(b_labels[i]), font=get_sig(b_labels[i]), cex=.8)
+  lines(c(x_med+6,x_med+7), rep(y_mediators[i], 2))
+}
+
+# --- indirect effects (a*b) ---
+for (i in seq_along(y_mediators)) {
+  text(x_med+box_w/2, y_mediators[i]+ind_offset, get_val(ind_labels[i]), 
+       font=get_sig(ind_labels[i]), cex=.8)
+}
+
+# --- direct effect (c/c') ---
+lines(rep(x_left+2,2),c(5.5, y_mediators[5]-1.75))
+lines(c(x_left+2,x_right+2), rep(y_mediators[5]-1.75, 2))
+Arrows(x_right+2, y_mediators[5]-1.75, x_right+2, y_mediators[3]-.5, arr.type="triangle", arr.adj=1, arr.length=.2, arr.width=.2)
+text(x_med+2,y_mediators[5]-1.75+.3, get_val("c"), font=get_sig("c"), cex=.8)
+text(x_med+2,y_mediators[5]-1.75-.3, paste0("[", get_val("total"), "]"), font=get_sig("total"), cex=.8)
+
+# reset plot margins
+par(mar=c(5,4,4,2))
+
+# now save figure as *.eps via Export > Save as Image ...
